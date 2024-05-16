123646d8fdde104d981365290e410230842c4b7c
YuejieLiu <yuejie.liu0104@gmail.com>
2024-05-16 14:52:19 +0200

    Update Porousity.py

--- Porousity.py
+++ Porousity.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def Porosity(d_cell, d_rod, scaling_factor, lattice_num):
-    v_lattice = np.sqrt(2)*np.pi*d_cell*np.power(d_rod,2)*np.pow(scaling_factor,2)*lattice_num
+    v_lattice = np.sqrt(3)*np.pi*d_cell*np.power(d_rod,2)*np.pow(scaling_factor,2)*lattice_num
     v_cube = np.power(d_cell,3)*np.pow(scaling_factor,2)*lattice_num
     v_plates = 1.5*np.power(d_cell,2)*np.power(scaling_factor,2)*lattice_num
     v_mesh = v_lattice + 2*v_plates
